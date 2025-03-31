19cb01cba5531021cca6e0af4b7086703c35eac8
jorge <toxicpotato1223@gmail.com>
2025-03-31 18:25:31 +0800

    added authorization for organization manager

--- app/Http/Controllers/OrganizationManagerController.php
+++ app/Http/Controllers/OrganizationManagerController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class OrganizationManagerController extends Controller
 {
     //
     public function show()
     {
-        return Inertia::render('OrganizationManager', []);
+        $UserId = auth()->id();
+ 
+        $UserRow = User::where('id', $UserId)->first()->makeVisible('is_admin')->toArray();
+      
+
+        return $UserRow['is_admin'] == 0 ? 
+            redirect('/dashboard') : 
+            Inertia::render('OrganizationManager', []);
     }
 }

9ccdd2de2e9b2a01946fe89585db3bcc0784dc8b
jorge <toxicpotato1223@gmail.com>
2025-04-12 13:24:19 +0800

    Now adds pending request when trying to join

--- app/Http/Controllers/JoinOrganizationController.php
+++ app/Http/Controllers/JoinOrganizationController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Models\Organizations;
 
 class JoinOrganizationController extends Controller
 {
@@ -14,8 +16,25 @@ class JoinOrganizationController extends Controller
 
     public function join(Request $request)
     {
-        
 
-        return back()->withErrors(['error' => 'Failed to join organization.']);
+        $userId = Auth::id();
+        $organizationUserWantsTOJoin = $request->toArray()['organizationSelected'];
+
+        $userRow = Organizations::where('user_id', $userId)
+            ->where('organization_name', $organizationUserWantsTOJoin)
+            ->first();
+
+        if (!$userRow) {
+     
+            Organizations::create([
+                'organization_name' => $organizationUserWantsTOJoin,
+                'user_id' => $userId,
+                'status' => 'pending',
+                ]
+            );
+            return back()->with('success', 'Successfully requested to join organization.');
+        }
+
+        return back()->withErrors('error', 'You have already requested to join or are a member.');      
     }
 }

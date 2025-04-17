50df7ca332fc742c65b193f27b63bdfb1f383727
jorge <toxicpotato1223@gmail.com>
2025-04-17 23:44:28 +0800

    added non searchbar

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -1,4 +1,4 @@
-import { Link } from "@inertiajs/react";
+import MagnifyingGlass from "../images/MagnifyingGlass.svg"
 import BackgroundImage from "../images/IndexBackground5.jpg";
 
 const Index = () => {
@@ -19,9 +19,33 @@ const Index = () => {
       <div 
         className="flex flex-col items-center justify-center w-screen h-[80vh] bg-cover bg-[position:center_90%]  bg-neutral-900"
         style={{ backgroundImage: `url(${BackgroundImage})` }}>
+      
+        <div className="flex flex-row items-center justify-center h-[12%] w-full bg-gray-900 bg-opacity-45">
+          {/* Input container */}
+          <div className="relative w-[40%]">
+            <label
+              htmlFor="search-input"
+              className="absolute left-1 top-1/2 -translate-y-1/2 text-white text-opacity-40 text-sm pointer-events-none">
+              <img src={MagnifyingGlass} alt="Search Icon" className="w-5 h-5 opacity-60 hover:opacity-100" />
+            </label>
+            <input
+              id="search-input"
+              type="text"
+              placeholder="Search..."  
+              className="w-full pl-7 pb-1 bg-transparent border-0 border-b border-white border-opacity-[40%]
+                text-sm text-white text-opacity-40 focus:text-opacity-100 hover:text-opacity-100
+                placeholder-white placeholder-opacity-40
+                focus:outline-none focus:border-b focus:border-white focus:ring-0 focus:border-opacity-100
+                hover:border-opacity-100"
+            />
+          </div>
+          <img src={MagnifyingGlass} alt="Search Icon" className="w-6 h-6 ml-3 mt-2" />
+        </div>
 
         <div className="flex flex-row items-center justify-center w-full h-full">
-            <div className="flex flex-col justify-center items-center w-[40%] h-full bg-opacity-50">
+            <div className="flex flex-col justify-center items-center w-[40%] h-full bg-opacity-50 ">
+
+              {/* Welcome Text */}
               <div className="flex flex-col items-center justify-start w-[90%] h-[90%] border border-red-500">
                 <div className="flex flex-col items-center justify-center w-[80%] h-[30%] mt-20 mr-16 border border-red-500">
                   <h1 className="text-6xl font-bold text-white font-roboto drop-shadow-lg">
